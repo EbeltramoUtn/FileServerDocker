8dd8b9f77eaadd782966ecc5423bd44250e4cd93
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-09-06 12:06:13 -0300

    create

---
+++ SaveFile/src/main/java/FileServer/SaveFile/SaveFileApplication.java
@@ -0,0 +1,13 @@
+package FileServer.SaveFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveFileApplication.class, args);
+	}
+
+}
