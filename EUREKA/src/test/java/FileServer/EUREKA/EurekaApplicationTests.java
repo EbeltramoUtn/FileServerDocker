c0e9662f5233e98a9b9c94896ed93d71e1346cfb
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-09-06 11:37:19 -0300

    Create Eureka

---
+++ EUREKA/src/test/java/FileServer/EUREKA/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package FileServer.EUREKA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
