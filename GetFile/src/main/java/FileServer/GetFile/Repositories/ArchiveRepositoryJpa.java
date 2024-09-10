6e94acc8c454c9df7ffebeaef47f3fad149780f1
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-09-10 16:56:26 -0300

    Update ArchiveRepositoryJpa.java

--- GetFile/src/main/java/FileServer/GetFile/Repositories/ArchiveRepositoryJpa.java
+++ GetFile/src/main/java/FileServer/GetFile/Repositories/ArchiveRepositoryJpa.java
@@ -3,7 +3,15 @@ package FileServer.GetFile.Repositories;
 import FileServer.GetFile.Entities.ArchiveEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ArchiveRepositoryJpa extends JpaRepository<ArchiveEntity, UUID> {
+    /**
+     * Retrieves an ArchiveEntity by its SHA-256 hash.
+     *
+     * @param hashSha256 the SHA-256 hash of the archive
+     * @return an Optional containing the ArchiveEntity if found, otherwise empty
+     */
+    Optional<ArchiveEntity> findByHashSha256(String hashSha256);
 }
