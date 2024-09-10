f13107d596883f428939330b1cca2cbc34fe4214
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-09-10 16:55:26 -0300

    Update FileDto.java
    add lombok

--- GetFile/src/main/java/FileServer/GetFile/Dtos/FileDto.java
+++ GetFile/src/main/java/FileServer/GetFile/Dtos/FileDto.java
@@ -1,11 +1,19 @@
 package FileServer.GetFile.Dtos;
 
+import lombok.*;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class FileDto {
     private UUID uuid;
     private String hashMd5;
     private String fileName;
     private String mimeType;
     private String extension;
+    private byte[] bytes;
 }
